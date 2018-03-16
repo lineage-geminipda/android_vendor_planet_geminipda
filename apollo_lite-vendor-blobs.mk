80e1690cfdb25cac7fd8f2649c0bf7df381224d1
deadman96385 <seanhoyt963@gmail.com>
2018-03-16 12:38:36 -0500

    Convert to gemini

--- apollo_lite-vendor-blobs.mk
+++
@@ -1,7 +0,0 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/usr,system/usr)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/vendor,system/vendor)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/vernee/apollo_lite/lib64,system/lib64)
