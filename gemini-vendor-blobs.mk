465152413cb5b051836697db1c45e2f517c17d7e
deadman96385 <seanhoyt963@gmail.com>
2018-03-20 14:57:48 -0500

    Rename to geminipda

--- gemini-vendor-blobs.mk
+++
@@ -1,7 +0,0 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/planet/gemini/usr,system/usr)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/planet/gemini/vendor,system/vendor)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/planet/gemini/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/planet/gemini/lib64,system/lib64)
