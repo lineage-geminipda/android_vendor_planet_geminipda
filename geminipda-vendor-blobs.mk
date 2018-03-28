8df0d2fbb028b6fd7194f6be433dbda776922e69
deadman96385 <seanhoyt963@gmail.com>
2018-03-28 08:58:59 -0500

    Regen vendor with extract scripts

--- geminipda-vendor-blobs.mk
+++
@@ -1,7 +0,0 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/planet/geminipda/usr,system/usr)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/planet/geminipda/vendor,system/vendor)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/planet/geminipda/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/planet/geminipda/lib64,system/lib64)
